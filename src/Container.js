afd3a610b360df75fbb4f6c36fff5279b46ff0f8
Thinh Nguyen <nguyendthinh@outlook.com>
2016-12-15 10:03:28 -0500

    did step 1 and 2

---
+++ src/Container.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Container extends Component {
+  render() {
+    return (
+      <h1>"I'm a container"</h1>
+    )
+  }
+}
+
+export default Container;
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Container from './Container.js'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HELLO WORLD</h1>
+        <Container />
+      </div>
+    );
+  }
+}
+
+export default Home;
